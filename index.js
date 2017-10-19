995aa68c743cd186add730fa5ef6ccee08453cef
dudick123 <brian.dudick@outlook.com>
2017-10-19 14:20:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World, I think? I think, again. ANd rebuild it.");
+    response.end("Hello World, I think? I think, again. And rebuild it.");
 
 });
 
